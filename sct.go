27b6c49aa7c504e727c3f3c608961a1577fce7a2
Rob Stradling <rob@sectigo.com>
2025-12-16 12:26:17 +0000

    Include log description when reporting an invalid SCT signature.

--- sct.go
+++ sct.go
@@ -31,15 +31,19 @@ func verifySCT(tbsCert []byte, sha256IssuerSPKI *[sha256.Size]byte, sct *ctgo.Si
 		return []string{"B: Signature verifier is not available"}
 	}
 
-	err := sv.VerifySCTSignature(*sct, ctgo.LogEntry{Leaf: merkleTreeLeaf})
-	if err != nil {
-		return []string{"E: SCT has an invalid signature"}
-	}
-
 	// Get the log description, for display purposes.  The crt.sh and gstatic log lists should cover all known logs between them.
 	log, _, _ := findLogByKeyHash(sct.LogID.KeyID, crtshV3AllLogsList)
 	if log == nil {
 		log, _, _ = findLogByKeyHash(sct.LogID.KeyID, gstaticV3AllLogsList)
+	}
+
+	err := sv.VerifySCTSignature(*sct, ctgo.LogEntry{Leaf: merkleTreeLeaf})
+	if err != nil {
+		if log != nil {
+			return []string{fmt.Sprintf("E: SCT has an invalid signature purporting to be from %s", log.Description)}
+		} else {
+			return []string{"E: SCT has an invalid signature"}
+		}
 	}
 
 	if log != nil {
